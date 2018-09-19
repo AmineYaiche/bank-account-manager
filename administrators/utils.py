41397a1362d93bea8716292b10f1bff33d339d19
Amine <yaiche.amin@gmail.com>
2018-09-19 22:46:22 +0100

    Fixing bug: Auth user
    Always authenticate the first user in the database due to the use of
    objects.first() is the authentication method.

--- administrators/utils.py
+++ administrators/utils.py
@@ -4,11 +4,6 @@ from django.contrib.auth.models import User
 from .models import Administrator
 
 
-def login_administrator(request, id_info):
-    administrator = Administrator.objects.first()
-    login(request, administrator.user)
-
-
 def login_or_create_administrator(request, id_info):
     administrator = Administrator.objects.filter(google_id=id_info.get('sub'))
     if not administrator.exists():
@@ -22,7 +17,8 @@ def login_or_create_administrator(request, id_info):
         )
         user.set_unusable_password()
         user.save()
-        Administrator.objects.create(google_id=id_info.get('sub'), user=user)
+        administrator = Administrator.objects.create(google_id=id_info.get('sub'), user=user)
+    else:
+        administrator = administrator.first()
 
-    login_administrator(request, id_info)
-
+    login(request, administrator.user)
